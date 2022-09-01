993c9a6543eacf54165062be8a37d00b6b4b1c62
Juliano_Ric <julianorics@hotmail.com>
2022-09-01 10:07:25 -0300

    atualizando o repo remoto com a mudanca local

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 
 console.log("mudança feita diretamente no repositorio hospedado no site github")
+ 
+//para atualizar o repo local (hospedado na maquina) --> git  pull
+
+console.log("mudança feita no repo local, favor atualizar via git push o repo remoto")
+
+//para atualizar remoto --> git add app.js --> git  commit -m "blablabla" --> git push
+
+
